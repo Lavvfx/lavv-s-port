fb96838f547fd7a38fbfad673b2c6f2995868ed0
Lavvfx <145562337+Lavvfx@users.noreply.github.com>
2024-06-29 11:16:27 +0530

    Update app.js
    Contact Form Function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,78 @@
+/*GSAP animation */
+
+
+gsap.from(".bg-img", 1,{
+  top: "1000",
+  opacity:0,
+ });
+ 
+ gsap.from(".sun-8", 1,{
+   top: "2000",
+   ease: "power4.0ut",
+   opacity:0,
+  });
+ 
+ gsap.from(".mountfuji-7", 1.2,{
+   top: "2000",
+   ease: "power4.0ut",
+  });
+ 
+  gsap.from(".cloud-11", 1.4,{
+   top: "2000",
+   ease: "power4.0ut",
+  });
+ 
+  gsap.from(".mountain-10", 1.5,{
+   top: "2000",
+   ease: "power4.0ut",
+  });
+ 
+  gsap.from(".cloud-9", 1.6,{
+   top: "2000",
+   ease: "power4.0ut",
+  });
+ 
+  gsap.from(".mountain-6", 1.8,{
+   top: "2000",
+   ease: "power4.0ut",
+  });
+ 
+  gsap.from(".mountain-5", 2.0,{
+   top: "2000",
+   ease: "power4.0ut",
+  });
+ 
+  gsap.from(".shrine-4", 2.2,{
+   top: "2000",
+   ease: "power4.0ut",
+  });
+ 
+  gsap.from(".fog-3", 2.4,{
+   top: "2000",
+   ease: "power4.0ut",
+   
+  });
+ 
+  gsap.from(".grass-2", 2.8,{
+   top: "2000",
+   ease: "power4.0ut",
+   
+  });
+ 
+  gsap.from(".grass-1", 2.6,{
+   top: "2000",
+   ease: "power4.0ut",
+  
+  });
+ 
+  gsap.from(".hide",{
+    opacity:0,
+    duration: 1.5,
+   }, "1.5");
+
+// parallax effect 
+
+
 const parallax_el = document.querySelectorAll(".parallax");
 
 let xValue = 0,
@@ -37,117 +112,13 @@ window.addEventListener("mousemove", (e) => {
   update(e.clientX);
 });
 
-/*GSAP animation */
-// function startloader() {
-// let counterElement = document.querySelector(".counter");
-// let currentValue = 0;
+// contact form function
 
-// function updateCounter(){
-//   if(currentValue === 100){
-//     return;
-//   }
-//   currentValue += Math.floor(Math.random() *10) + 1;
-  
-//   if(currentValue > 100){
-//     currentValue = 100;
-//   }
-//   counterElement.textContent = currentValue;
-
-//   let delay = Math.floor(Math.random() * 200) + 50;
-//   setTimeout(updateCounter, delay);
-// }
-
-// updateCounter();
-
-// }
+document.querySelector(".button").addEventListener("click", function() {
+  document.querySelector(".popup").style.display = "flex";
+});
 
 
-// startloader();
-
-// gsap.to(".counter", 0.25,{
-//   delay:3.5,
-//   opacity:0,
-// });
-
-// gsap.to(".bar", 1,{
-//  delay: 3.5,
-//  height: 0,
-//  stagger: {
-//   amount: 0.5,
-//  },
-//  ease: "power4.in0ut",
-// });
-
-  gsap.from(".bg-img", 1,{
-    top: "1000",
-    opacity:0,
-   });
-   
-   gsap.from(".sun-8", 1,{
-     top: "2000",
-     ease: "power4.0ut",
-     opacity:0,
-    });
-   
-   gsap.from(".mountfuji-7", 1.2,{
-     top: "2000",
-     ease: "power4.0ut",
-    });
-   
-    gsap.from(".cloud-11", 1.4,{
-     top: "2000",
-     ease: "power4.0ut",
-    });
-   
-    gsap.from(".mountain-10", 1.5,{
-     top: "2000",
-     ease: "power4.0ut",
-    });
-   
-    gsap.from(".cloud-9", 1.6,{
-     top: "2000",
-     ease: "power4.0ut",
-    });
-   
-    gsap.from(".mountain-6", 1.8,{
-     top: "2000",
-     ease: "power4.0ut",
-    });
-   
-    gsap.from(".mountain-5", 2.0,{
-     top: "2000",
-     ease: "power4.0ut",
-    });
-   
-    gsap.from(".shrine-4", 2.2,{
-     top: "2000",
-     ease: "power4.0ut",
-    });
-   
-    gsap.from(".fog-3", 2.4,{
-     top: "2000",
-     ease: "power4.0ut",
-     
-    });
-   
-    gsap.from(".grass-2", 2.8,{
-     top: "2000",
-     ease: "power4.0ut",
-     
-    });
-   
-    gsap.from(".grass-1", 2.6,{
-     top: "2000",
-     ease: "power4.0ut",
-    
-    });
-   
-    gsap.from(".hide",{
-      opacity:0,
-      duration: 1.5,
-     }, "1.5");
-
-
-
- 
-
+document.getElementById("close").addEventListener("click", function() {
+  document.querySelector(".popup").style.display = "none";
+});
